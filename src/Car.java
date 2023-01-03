f9cda165f493e8313bf194bf88fefa15e1375045
yarosla7 <yanin.yarosla7@gmail.com>
2023-01-03 19:48:07 +0300

    третья задача решена

--- src/Car.java
+++ src/Car.java
@@ -7,16 +7,42 @@ public class Car {
     String country;
 
     Car(String brand, String model, double engineVolume, String color, int year, String country) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.year = year;
-        this.country = country;
+
+        if (brand.equals(" ") || brand.isBlank()) {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+        if (model.equals(" ") || model.isBlank()) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+        if (color.equals(" ") || color.isBlank()) {
+            this.color = "white";
+        } else {
+            this.color = color;
+        }
+        if (country.equals(" ") || country.isBlank()) {
+            this.country = "default";
+        } else {
+            this.country = country;
+        }
+        if (engineVolume >= 0) {
+            this.engineVolume = engineVolume;
+        } else {
+            this.engineVolume = 1.5;
+        }
+        if (year >= 0) {
+            this.year = year;
+        } else {
+            this.year = 2000;
+        }
     }
+
 
     @Override
     public String toString() {
-        return brand + " " + model + ", " + year + " года выпуска, сборка в " + country + ", " + color + " цвет кузова, объём двигателя — " + engineVolume + " л.";
+        return brand + " " + model + ", " + year + " — realise year, manufacturer country is " + country + ", body color is " + color + ", engine volume — " + engineVolume + " liters.";
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Car lada = new Car("Lada", "Granta", 1.7, "yellow", 2015, "Russia");
+        Car lada = new Car("Lada", "Granta", 1.7," ", 2015, " ");
         System.out.println("lada.toString() = " + lada.toString());
         Car audi = new Car("Audi", "A8 50L TDI quattro", 3.0, "black", 2020, "Germany");
         System.out.println("audi.toString() = " + audi.toString());
@@ -10,5 +10,7 @@ public class Main {
         System.out.println("kia.toString() = " + kia.toString());
         Car hyundai = new Car("Hyundai", "Avante", 1.6, "orange", 2016, "South Korea");
         System.out.println("hyundai.toString() = " + hyundai.toString());
+        Car myCar = new Car("","",0,"",-100,"");
+        System.out.println("emptyCar.toString() = " + myCar.toString());
     }
 }
