8ba6eb5f0b6601b0aca779c58b1a36a69494ee86
yarosla7 <yanin.yarosla7@gmail.com>
2023-01-03 19:09:50 +0300

    вторая задача решена

--- src/Car.java
+++ src/Car.java
@@ -6,6 +6,15 @@ public class Car {
     int year;
     String country;
 
+    Car(String brand, String model, double engineVolume, String color, int year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
+
     @Override
     public String toString() {
         return brand + " " + model + ", " + year + " года выпуска, сборка в " + country + ", " + color + " цвет кузова, объём двигателя — " + engineVolume + " л.";
--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Car lada = new Car();
-        lada.brand = "Lada";
-        lada.model = "Granta";
-        lada.year = 2015;
-        lada.country = "Russia";
-        lada.color = "жёлтый";
-        lada.engineVolume = 1.7;
+        Car lada = new Car("Lada", "Granta", 1.7, "yellow", 2015, "Russia");
         System.out.println("lada.toString() = " + lada.toString());
-        Car audi = new Car();
-        audi.brand = "Audi";
-        audi.model = "A8 50L TDI quattro";
-        audi.year = 2020;
-        audi.country = "Germany";
-        audi.color = "черный";
-        audi.engineVolume = 3.0;
+        Car audi = new Car("Audi", "A8 50L TDI quattro", 3.0, "black", 2020, "Germany");
         System.out.println("audi.toString() = " + audi.toString());
-        Car bmw = new Car();
-        bmw.brand = "BMW";
-        bmw.model = "Z8";
-        bmw.year = 2021;
-        bmw.country = "Germany";
-        bmw.color = "черный";
-        bmw.engineVolume = 3.0;
+        Car bmw = new Car("BMW", "Z8", 3.0, "black", 2021, "Germany");
         System.out.println("bmw.toString() = " + bmw.toString());
-        Car kia = new Car();
-        kia.brand = "Kia";
-        kia.model = "Sportage 4th generation";
-        kia.year = 2018;
-        kia.country = "South Korea";
-        kia.color = "красный";
-        kia.engineVolume = 2.4;
+        Car kia = new Car("Kia", "Sportage 4th generation", 2.4, "red", 2018, "South Korea");
         System.out.println("kia.toString() = " + kia.toString());
-        Car hyundai = new Car();
-        hyundai.brand = "Hyundai";
-        hyundai.model = "Avante";
-        hyundai.year = 2016;
-        hyundai.country = "South Korea";
-        hyundai.color = "оранжевый";
-        hyundai.engineVolume = 1.6;
+        Car hyundai = new Car("Hyundai", "Avante", 1.6, "orange", 2016, "South Korea");
         System.out.println("hyundai.toString() = " + hyundai.toString());
     }
 }
